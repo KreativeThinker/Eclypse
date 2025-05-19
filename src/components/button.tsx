72c670783f5108c3e7ebc9faf6bde062afe3730a
KreativeThinker <sagely2005eagle@gmail.com>
2025-05-19 18:21:33 +0530

    chore: add hover styles

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,9 +5,18 @@ type ButtonProps = {
 }
 const Button: React.FC<ButtonProps> = ({ children, onClick, variant = 'primary' }) => {
   const variantStyle =
-    variant === 'primary' ? 'bg-foreground text-background' : 'bg-background text-foreground'
+    variant === 'primary'
+      ? 'bg-foreground text-background'
+      : 'bg-background text-foreground border border-background'
+  const hoverStyle =
+    variant === 'primary'
+      ? 'hover:bg-accent hover:text-foreground'
+      : 'hover:bg-foreground hover:text-background'
   return (
-    <button className={`${variantStyle} rounded-md px-6 py-2 transition duration-200`}>
+    <button
+      className={`${variantStyle} ${hoverStyle} rounded-md px-6 py-2 transition-all duration-200 ease-in-out`}
+      onClick={onClick}
+    >
       {children}
     </button>
   )
