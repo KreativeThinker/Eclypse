7fa1d5a67dca485293faae314b3e39bc91d9c257
KreativeThinker <sagely2005eagle@gmail.com>
2025-05-21 00:57:35 +0530

    chore: responsive

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -11,11 +11,11 @@ type CardProps = {
 
 export default function Card({ imageSrc, alt, overlayText }: CardProps) {
   return (
-    <div className="group relative aspect-square h-full w-full overflow-hidden rounded-sm">
+    <div className="group relative aspect-square h-full w-full overflow-hidden rounded-sm lg:rounded-md">
       <Image src={imageSrc} alt={alt} fill className="object-cover" />
       <div className="absolute inset-0 bg-[#36363699] opacity-0 transition-opacity group-hover:opacity-100" />
-      <div className="text-foreground absolute bottom-2 left-0 w-full px-2 opacity-0 transition-opacity group-hover:opacity-100">
-        <p className="text-sm">{overlayText}</p>
+      <div className="text-foreground absolute bottom-2 left-0 w-full px-2 opacity-0 transition-opacity group-hover:opacity-100 md:bottom-4 md:px-4">
+        <p className="text-sm md:text-xl lg:text-4xl">{overlayText}</p>
       </div>
     </div>
   )
