191c9665d44fc0df5d5db458c29bc392cdb52c2a
KreativeThinker <sagely2005eagle@gmail.com>
2025-05-20 19:20:28 +0530

    feat: faq component

---
+++ src/components/faq.tsx
@@ -0,0 +1,16 @@
+import arrow from '../../public/arrow.svg'
+import Image from 'next/image'
+
+type FAQProps = {
+  question: string
+  answer?: string
+}
+
+export default function FAQ({ question, answer }: FAQProps) {
+  return (
+    <div className="border-neutral-4 flex flex-row items-center justify-between border-b py-4">
+      <h3 className="text-lg font-semibold">{question}</h3>
+      <Image src={arrow} alt="arrow" className="rotate-135" />
+    </div>
+  )
+}
