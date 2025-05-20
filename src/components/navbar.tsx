2d8235538b27c3a8f363fa3cbeda18bffca67a84
KreativeThinker <sagely2005eagle@gmail.com>
2025-05-20 14:13:31 +0530

    chore: navbar resizing

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,13 +4,13 @@ import Button from './button'
 
 export default function Navbar() {
   return (
-    <nav className="bg-background/60 sticky top-0 z-50 flex flex-row items-center px-4 py-8 backdrop-blur-xl">
+    <nav className="bg-background/60 sticky top-0 z-50 flex flex-row items-center px-8 py-2 backdrop-blur-xl">
       <div>
         <a href="/">
-          <img src="/assets/logo.jpg" className="h-10" />
+          <img src="/assets/logo.jpg" className="h-6" />
         </a>
       </div>
-      <div className="mt-auto mb-auto flex w-full flex-row items-center justify-end gap-8">
+      <div className="mt-auto mb-auto flex w-full flex-row items-center justify-end gap-8 text-xs">
         <a href="/about">About Us</a>
         <a href="/services">Waitlist</a>
         <a href="/contact">Cart</a>
