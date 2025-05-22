39d5df1e5326332736c2bff390b36e2795953c0a
KreativeThinker <sagely2005eagle@gmail.com>
2025-05-22 16:28:07 +0530

    feat: backend router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,26 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
+import productRouter from "./routes/product";
+import testimonialRouter from "./routes/testimonials";
+import checkoutRouter from "./routes/buy";
+
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 4000;
+const apiRouter = express.Router();
+
+app.use(cors());
+app.use(express.json());
+
+apiRouter.use("/product", productRouter);
+apiRouter.use("/testimonials", testimonialRouter);
+apiRouter.use("/checkout", checkoutRouter);
+
+app.use("/api", apiRouter);
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
