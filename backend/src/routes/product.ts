f0def4ae1e630f453458134eb0c88e6cf58434eb
KreativeThinker <sagely2005eagle@gmail.com>
2025-05-22 17:00:53 +0530

    chore: types

--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -1,24 +1,27 @@
 import { Router } from "express";
+import type { Product } from "@shared/types/index";
 
 const productRouter = Router();
 
+const product: Product = {
+  id: "S1",
+  name: "Silhouette No. 1",
+  price: 7999,
+  sizes: ["XS", "S", "M", "L", "XL"],
+  media: {
+    video: "/media/videos/video-3.mp4",
+    images: [
+      "/media/images/product-side.png",
+      "/media/images/product-back.png",
+      "/media/images/product-back-long.png",
+    ],
+  },
+  description:
+    "A tailored composition in motion. Cut from structured wool with a sculpted shoulder and softened hem, this piece captures presence without force. Worn here in the stillness of a city in motion.",
+};
+
 productRouter.get("/", (req, res) => {
-  res.json({
-    id: "cloth-1",
-    name: "Eclypse Overshirt",
-    price: 4999,
-    sizes: ["S", "M", "L", "XL"],
-    media: {
-      video: "/media/videos/video-3.mp4",
-      images: [
-        "/media/images/product-side.png",
-        "/media/images/product-back.png",
-        "/media/images/product-back-long.png",
-      ],
-    },
-    description:
-      "Structured utility overshirt with reinforced stitchwork and curved collar.",
-  });
+  res.json(product);
 });
 
 export default productRouter;
--- backend/src/routes/testimonials.ts
+++ backend/src/routes/testimonials.ts
@@ -1,21 +1,28 @@
 import { Router } from "express";
+import type { Testimonial } from "../../../shared/types";
 
 const testimonialRouter = Router();
 
-const testimonials = [
+const testimonials: Testimonial[] = [
   {
+    id: 1,
+    productid: "S1",
     name: "Random Woman",
     location: "NY, USA",
     text: "Understated, but unforgettable. It feels like it was made for me",
     avatar: "/media/avatars/woman1.jpg",
   },
   {
+    id: 2,
+    productid: "S1",
     name: "Guy with Glasses",
     location: "CA, USA",
     text: "It changed how I feel about clothing.",
     avatar: "/media/avatars/man1.jpg",
   },
   {
+    id: 3,
+    productid: "S1",
     name: "Cool Woman",
     location: "TX, USA",
     text: "Minimal, stylish, perfect.",
