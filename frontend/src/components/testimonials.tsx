58447821fddf41157ba734f45a4cfa64d24f40bd
KreativeThinker <sagely2005eagle@gmail.com>
2025-05-22 16:45:16 +0530

    chore: types

--- frontend/src/components/testimonials.tsx
+++ frontend/src/components/testimonials.tsx
@@ -3,30 +3,10 @@
 import BASE_URL from '@/utils/api'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
-
-const testimonialsFallback = [
-  {
-    name: 'Random Woman',
-    location: 'NY, USA',
-    text: 'Understated, but unforgettable. It feels like it was made for me',
-    avatar: '/media/avatars/woman1.jpg',
-  },
-  {
-    name: 'Guy with Glasses',
-    location: 'CA, USA',
-    text: 'It changed how I feel about clothing.',
-    avatar: '/media/avatars/man1.jpg',
-  },
-  {
-    name: 'Cool Woman',
-    location: 'TX, USA',
-    text: 'Minimal, stylish, perfect.',
-    avatar: '/media/avatars/woman2.jpg',
-  },
-]
+import type { Testimonial } from '../../../shared/types/index'
 
 export default function TestimonialSlider() {
-  const [testimonials, setTestimonials] = useState(testimonialsFallback)
+  const [testimonials, setTestimonials] = useState<Testimonial[]>([])
   useEffect(() => {
     fetch(`${BASE_URL}/testimonials`)
       .then((res) => res.json())
@@ -46,11 +26,11 @@ export default function TestimonialSlider() {
           <div className="mb-6 flex flex-row">
             <span className="mr-2 align-sub text-4xl md:text-9xl">“</span>
             <div className="font-montreal text-neutral-3 max-w-3/4 text-sm md:max-w-[60%]">
-              <h4 className="text-2xl md:text-5xl">{testimonials[active].text}</h4>
+              <h4 className="text-2xl md:text-5xl">{testimonials[active]?.text}</h4>
               <p className="text-neutral-3 font-montreal mt-4 md:mt-24 md:text-xl">
-                {testimonials[active].name}
+                {testimonials[active]?.name}
               </p>
-              <p className="text-neutral-1 font-montreal">{testimonials[active].location}</p>
+              <p className="text-neutral-1 font-montreal">{testimonials[active]?.location}</p>
             </div>
           </div>
         </div>
@@ -58,7 +38,7 @@ export default function TestimonialSlider() {
         <div className="flex flex-col items-center gap-4">
           <div className="relative h-16 w-16 md:h-30 md:w-30">
             <Image
-              src={testimonials[active].avatar}
+              src={testimonials[active]?.avatar}
               alt="avatar"
               className="rounded-full object-cover"
               fill
