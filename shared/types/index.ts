69dcc9b52da9c90c968a8f03f6d441b13b193bf9
KreativeThinker <sagely2005eagle@gmail.com>
2025-05-22 16:45:27 +0530

    feat: add shared type declarations

---
+++ shared/types/index.ts
@@ -0,0 +1,22 @@
+type Testimonial = {
+  id: number;
+  productid: string;
+  name: string;
+  location: string;
+  text: string;
+  avatar: string;
+};
+
+type Product = {
+  id: string;
+  name: string;
+  price: number;
+  sizes: string[];
+  media: {
+    video: string;
+    images: string[];
+  };
+  description: string;
+};
+
+export type { Testimonial, Product };
